2264477c44b8ee1c794eeadd84e02add5ceace24
ErosEros <eros.vittori1994@gmail.com>
2022-09-05 01:03:27 +0200

    add image html

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 // layout di immagini che si alternano
 
 // 2 bottoni che permetteno di scorrere le immagini avanti e dietro
+
+// visualizzo un immagine alla volta ( mentre le altre sono nascoste)
